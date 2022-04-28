37de77eaeaf1a894193760b2173f38f482d42250
Di <s3926977@rmit.edu.vn>
2022-04-28 15:25:37 +0700

    Test pages

---
+++ index.php
@@ -0,0 +1,11 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Index</title>
+    </head>
+    <body>
+        <p>Congratulation! You have logged into password protected page. <a href="logout.php">Click here</a> to Logout.</p>
+    </body>
+
+</html>
